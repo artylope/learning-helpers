fbc31fd721fb0cfd2cef0970a6ae9b7be9d1a7fb
angyixin <angyixin@gmail.com>
2025-03-06 12:08:21 +0800

    container padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,53 +13,55 @@ export default function Home() {
     <div className="container mx-auto">
       <h1 className="text-4xl font-semibold my-8">Learning Helpers</h1>
 
-      <div className="my-6 space-y-4 flex flex-col gap-2 border rounded-lg p-4">
-        <h2 className="text-2xl font-semibold">Settings</h2>
-        <div className="flex flex-col gap-3 text-sm">
-          <h3 className="font-semibold">Text display</h3>
-          <div className="flex items-center space-x-2">
-            <Switch
-              id="show-pinyin"
-              checked={showPinyin}
-              onCheckedChange={setShowPinyin}
-            />
-            <label htmlFor="show-pinyin">Show pinyin for Chinese</label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <Switch
-              id="show-translation"
-              checked={showTranslation}
-              onCheckedChange={setShowTranslation}
-            />
-            <label htmlFor="show-translation">Show translation</label>
-          </div>
-        </div>{' '}
-        <div className="flex flex-col gap-3 text-sm">
-          <h3 className="font-semibold">Reading</h3>
-          <div className="flex items-center space-x-2 ">
-            <Switch
-              id="read-aloud"
-              checked={readAloud}
-              onCheckedChange={setReadAloud}
-            />
-            <label htmlFor="read-aloud">Show button to read aloud</label>
-          </div>
-          {readAloud && (
+      <div className="my-6 space-y-5 flex flex-col border rounded-lg p-4 pb-6">
+        <h2 className="text-xl font-semibold">Settings</h2>
+        <div className="space-y-4">
+          <div className="flex flex-col gap-3 text-sm">
+            <h3 className="font-semibold">Text display</h3>
             <div className="flex items-center space-x-2">
               <Switch
-                id="show-highlight"
-                checked={showSpeechHighlight}
-                onCheckedChange={setShowSpeechHighlight}
+                id="show-pinyin"
+                checked={showPinyin}
+                onCheckedChange={setShowPinyin}
               />
-              <label htmlFor="show-highlight">
-                Highlight words when it is being read aloud
-              </label>
+              <label htmlFor="show-pinyin">Show pinyin for Chinese</label>
             </div>
-          )}
+            <div className="flex items-center space-x-2">
+              <Switch
+                id="show-translation"
+                checked={showTranslation}
+                onCheckedChange={setShowTranslation}
+              />
+              <label htmlFor="show-translation">Show translation</label>
+            </div>
+          </div>{' '}
+          <div className="flex flex-col gap-3 text-sm">
+            <h3 className="font-semibold">Reading</h3>
+            <div className="flex items-center space-x-2 ">
+              <Switch
+                id="read-aloud"
+                checked={readAloud}
+                onCheckedChange={setReadAloud}
+              />
+              <label htmlFor="read-aloud">Show button to read aloud</label>
+            </div>
+            {readAloud && (
+              <div className="flex items-center space-x-2">
+                <Switch
+                  id="show-highlight"
+                  checked={showSpeechHighlight}
+                  onCheckedChange={setShowSpeechHighlight}
+                />
+                <label htmlFor="show-highlight">
+                  Highlight words when it is being read aloud
+                </label>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
-      <div className="my-6 space-y-4">
+      <div className="my-6 space-y-4 px-2">
         <WordCard
           phrase="我有一个梦想, 你呢？"
           translation="I have a dream, what's yours?"
