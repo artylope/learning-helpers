851e6baae544b832b4315082f84f3cc5d6f6166e
angyixin <angyixin@gmail.com>
2025-03-06 10:12:38 +0800

    remove image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
-    <div>
+    <div className="container mx-auto">
       <h1 className="text-13xl font-semibold">The best way to get started</h1>
     </div>
   );
