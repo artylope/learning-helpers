5711e792ada3e17386c80d80d1a761c097b83309
angyixin <angyixin@gmail.com>
2025-03-06 11:02:48 +0800

    settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,39 +13,41 @@ export default function Home() {
     <div className="container mx-auto">
       <h1 className="text-4xl font-semibold my-8">Learning Helpers</h1>
 
-      <div className="my-6 space-y-4 flex flex-col gap-2 border rounded-lg p-4">
+      <div className="my-6 space-y-2 flex flex-col gap-2 border rounded-lg p-4">
         <h2 className="text-xl font-semibold">Settings</h2>
-        <div className="flex items-center space-x-2">
-          <Switch
-            id="read-aloud"
-            checked={readAloud}
-            onCheckedChange={setReadAloud}
-          />
-          <label htmlFor="read-aloud">Able to read aloud</label>
-        </div>
-        <div className="flex items-center space-x-2">
-          <Switch
-            id="show-pinyin"
-            checked={showPinyin}
-            onCheckedChange={setShowPinyin}
-          />
-          <label htmlFor="show-pinyin">Show pinyin for Chinese</label>
-        </div>
-        <div className="flex items-center space-x-2">
-          <Switch
-            id="show-translation"
-            checked={showTranslation}
-            onCheckedChange={setShowTranslation}
-          />
-          <label htmlFor="show-translation">Show translation</label>
-        </div>
-        <div className="flex items-center space-x-2">
-          <Switch
-            id="show-highlight"
-            checked={showSpeechHighlight}
-            onCheckedChange={setShowSpeechHighlight}
-          />
-          <label htmlFor="show-highlight">Show speech highlight</label>
+        <div className="flex flex-col gap-3 text-sm">
+          <div className="flex items-center space-x-2 ">
+            <Switch
+              id="read-aloud"
+              checked={readAloud}
+              onCheckedChange={setReadAloud}
+            />
+            <label htmlFor="read-aloud">Able to read aloud</label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Switch
+              id="show-pinyin"
+              checked={showPinyin}
+              onCheckedChange={setShowPinyin}
+            />
+            <label htmlFor="show-pinyin">Show pinyin for Chinese</label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Switch
+              id="show-translation"
+              checked={showTranslation}
+              onCheckedChange={setShowTranslation}
+            />
+            <label htmlFor="show-translation">Show translation</label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Switch
+              id="show-highlight"
+              checked={showSpeechHighlight}
+              onCheckedChange={setShowSpeechHighlight}
+            />
+            <label htmlFor="show-highlight">Show speech highlight</label>
+          </div>
         </div>
       </div>
 
